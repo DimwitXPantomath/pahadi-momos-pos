ad439a61b9ed8238c9c64f7c820c767344878c48
Suraj Ahirwar <hackkers@Surajs-MacBook-Air.local>
2026-02-18 23:50:21 +0530

    remove illegal env assignment

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL="https://xvdagtgqvrqdqhpsfbtt.supabase.co" as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2ZGFndGdxdnJxZHFocHNmYnR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NTE3NjksImV4cCI6MjA4NDQyNzc2OX0.rUNTOdmdVPQ4nmVtqodPfsKNkxvjhyo3uJOTPXONEeo" as string;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
